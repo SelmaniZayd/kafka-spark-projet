6d511e04d705f3d3ece89995999d2a1cbb8b022d
zayd selmani <selmani.zayd@gmail.com>
2021-02-04 19:25:00 +0100

    init commit

---
+++ producer.py
@@ -0,0 +1,20 @@
+from time import sleep
+import json
+from json import dumps
+from kafka import KafkaProducer
+import pandas as pd
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: dumps(x).encode('iso-8859-15')
+)
+
+df = pd.read_csv("netflix_titles.csv")
+res = df.to_json(orient="records")
+y = json.loads(res)
+
+for i in y:
+    print("sent")
+    data = i
+    producer.send("netflix_movies", value=data)
+    sleep(2)
